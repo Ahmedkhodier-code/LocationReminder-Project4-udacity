98588d70cb6b96139f757a566a7e11eb7c422736
Ahmed Khodier <70490707+Ahmedkhodier-code@users.noreply.github.com>
2022-11-09 18:24:32 +0200

    newUpdate

--- app/src/androidTest/java/com/udacity/project4/util/FakeDataSource.kt
+++ app/src/androidTest/java/com/udacity/project4/util/FakeDataSource.kt
@@ -3,6 +3,7 @@ package com.udacity.project4.util
 
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
 
 class FakeDataSource(var tasks: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
     private var shouldReturnError = false
